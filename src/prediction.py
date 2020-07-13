a17430709002fb1b670d7b5f264d1f5094a3ff2f
Shubham <shubhamsaxena915@gmail.com>
2020-07-13 20:31:18 +0530

    Gradio support added, fixed files imports

--- src/prediction.py
+++ src/prediction.py
@@ -1,9 +1,10 @@
 from tensorflow import keras
 from tensorflow.keras.preprocessing import sequence
-
+import os
 import pickle
-model = keras.models.load_model('../models/model-lstm/model-lstm.h5')
-with open('../models/model-lstm/tokenizer.pkl', 'rb') as f:
+
+model = keras.models.load_model('models/model-lstm/model-lstm.h5')
+with open('models/model-lstm/tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f) 
 # print(model.summary())
 
@@ -11,14 +12,19 @@ with open('../models/model-lstm/tokenizer.pkl', 'rb') as f:
 def predict_probability(s):
 
     tokens = tokenizer.texts_to_sequences([s])
-
     tokens = sequence.pad_sequences(tokens, maxlen=1000)
-
     return model.predict(tokens)
 
 
 def predict_sentiment(s):
+    """Predicting Sentiments given a Review.
 
+    Args:
+        s (str): Review text
+
+    Returns:
+        sentiment: positive/negative
+    """
     pred = predict_probability(s)[0][0]
     print(s, pred)
     return "negative" if pred < 0.5 else "positive"
