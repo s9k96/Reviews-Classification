19de82a2c4d856c9b1a4a6930b24e79234c782f5
Shubham <shubhamsaxena915@gmail.com>
2020-08-05 16:49:24 +0530

    fasttext exp1

--- src/train.py
+++ src/train.py
@@ -3,9 +3,26 @@ import torch
 import pandas as pd
 import config
 import dataset
-import io
+import io, os
 import psutil 
 from tokenizer import Tokenizer
+from pprint import pprint
+import numpy as np
+
+def create_embedding_matrix(word_index, embedding_dict):
+    """
+    This function creates the embedding matrix.   
+    :param word_index: a dictionary with word:index_value   
+    :param embedding_dict: a dictionary with word:embedding_vector   
+    :return: a numpy array with embedding vectors for all known words 
+    """   
+    embedding_matrix = np.zeros((len(word_index) + 1, 300))
+    for word, i in word_index.items():         
+        if word in embedding_dict:          
+               embedding_matrix[i] = embedding_dict[word]
+    return embedding_matrix 
+ 
+ 
 
 def load_vectors(fname):
     # taken from: https://fasttext.cc/docs/en/english-vectors.html
@@ -39,8 +56,8 @@ def run(df, fold):
     xtest = tokenizer.texts_to_sequences(valid_df.review.values)
 
 
-    xtrain = tokenizer.pad_sequences(xtrain, max_len = 1805)
-    xtest = tokenizer.pad_sequences(xtest, max_len = 1805)
+    xtrain = tokenizer.pad_sequences(xtrain, max_len = 200)
+    xtest = tokenizer.pad_sequences(xtest, max_len = 200)
     print(xtrain.shape)
     print(xtest.shape)
 
@@ -67,8 +84,14 @@ def run(df, fold):
     )    
     print(psutil.virtual_memory())
 
-    del df, xtrain, xtest
-    # embedding_dict = load_vectors('../models/crawl-300d-2M.vec')
+    if not os.path.exists("../data/embedding_matrix.npy"):
+        embedding_dict = load_vectors('../models/crawl-300d-2M.vec')
+        print("Loaded Vectors")
+        embedding_matrix = create_embedding_matrix(tokenizer.word_index, embedding_dict)
+        print("Created Matrix")
+
+        np.save("../data/embedding_matrix.npy", embedding_matrix)
+
 
 if __name__ == '__main__':
     print(psutil.virtual_memory())
