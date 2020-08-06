39a11e04ca5c49f4092ff1cb993152f112a9d71d
Shubham <shubhamsaxena915@gmail.com>
2020-08-06 20:46:45 +0530

    Added scriptt for word2vec

---
+++ tools/get-word2vec-vectors.sh
@@ -0,0 +1,4 @@
+# https://stats.stackexchange.com/questions/267169/how-to-use-pre-trained-word2vec-model
+wget http://vectors.nlpl.eu/repository/20/3.zip -P ../models
+unzip ../models/3.zip -d ../models
+rm ../models/3.zip
